ab06c73ce2f3dd6b3f3733660c374b2d07b65604
Gina De La Rosa <g26delarosa@gmail.com>
2017-11-12 23:56:19 -0800

    updated UI

---
+++ Metis/HelperFiles/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Metis
+//
+//  Created by Gina De La Rosa on 11/12/17.
+//  Copyright © 2017 Gina Delarosa. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
+let KEY_UID = "uid"
+
