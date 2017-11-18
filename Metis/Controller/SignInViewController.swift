3ce59f162477a58cb54f15101ce7bbef352f581f
Gina De La Rosa <g26delarosa@gmail.com>
2017-11-18 12:16:43 -0800

    final push

--- Metis/Controller/SignInViewController.swift
+++ Metis/Controller/SignInViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import Firebase
 
 class SignInViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
@@ -88,43 +89,37 @@ class SignInViewController: UIViewController, UICollectionViewDelegate, UICollec
         })
     }
     
-//        @IBAction func facebookLogin(sender: UIButton) {
-//            let fbLoginManager = FBSDKLoginManager()
-//            fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
-//                if let error = error {
-//                    print("Failed to login: \(error.localizedDescription)")
-//                    return
-//                }
-//
-//                guard let accessToken = FBSDKAccessToken.current() else {
-//                    print("Failed to get access token")
-//                    return
-//                }
-//
-//                //let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
-//
-//                // Perform login by calling Firebase APIs
-//               // Auth.auth().signIn(with: credential, completion: { (user, error) in
-//                    if let error = error {
-//                        print("Login error: \(error.localizedDescription)")
-//                        let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
-//                        let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-//                        alertController.addAction(okayAction)
-//                        self.present(alertController, animated: true, completion: nil)
-//
-//                        return
-//                    }
-//                    // Present the main view
-//                    if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "HomeVC") {
-//                        UIApplication.shared.keyWindow?.rootViewController = viewController
-//                        self.dismiss(animated: true, completion: nil)
-//                    }
-//
-//                })
-//
-//            }
-//        }
-    
+        @IBAction func facebookLogin(sender: UIButton) {
+            let fbLoginManager = FBSDKLoginManager()
+            fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+                if let error = error {
+                    print("Failed to login: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let accessToken = FBSDKAccessToken.current() else {
+                    print("Failed to get access token")
+                    return
+                }
+
+                let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+
+                // Perform login by calling Firebase APIs
+               Auth.auth().signIn(with: credential, completion: { (user, error) in
+                    if let error = error {
+                        print("Login error: \(error.localizedDescription)")
+                        let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                        let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                        alertController.addAction(okayAction)
+                        self.present(alertController, animated: true, completion: nil)
+                        return
+                    }
+                // Present the main view
+                self.performSegue(withIdentifier: "SignInToTabbarVC", sender: nil)
+               })
+            }
+        }
+
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
                 return self.imageArray.count
             }
